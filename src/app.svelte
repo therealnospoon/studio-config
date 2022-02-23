7e7a11cb070c4e9c7d193f1523fcee402ad4ede0
Tim Kim <47040860+timkimdesigns@users.noreply.github.com>
2022-02-22 20:38:46 -0800

    Initial commit

---
+++ src/app.svelte
@@ -0,0 +1,14 @@
+<main class="container">
+    <div class="row">
+        <div class="col-12">
+            <img src="img/avatar.png" alt="avatar"/>
+        </div>
+        <div class="col-12">
+            <h3>Sup, i think it worked.</h3>
+        </div>
+        <div class="col-12">
+            <p>Thanks for using my template,</p>
+            <p>qudo</p>
+        </div>
+    </div>
+</main>
